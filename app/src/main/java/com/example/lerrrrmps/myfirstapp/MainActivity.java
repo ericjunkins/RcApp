234e9ae84acd13c75dda3da5a2578114d23c5c24
ericjunkins <eric.junkins>
2017-03-24 21:35:24 -0600

    Updating the .XML file for formatting the android app, fixed 2 joystick movement constraints, as well as added a few new buttons and positioned everything relative to where it should go in the final part roughly.

--- app/src/main/java/com/example/lerrrrmps/myfirstapp/MainActivity.java
+++ app/src/main/java/com/example/lerrrrmps/myfirstapp/MainActivity.java
@@ -1,16 +1,22 @@
 package com.example.lerrrrmps.myfirstapp;
 
+import android.app.ActionBar;
 import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.example.lerrrrmps.myfirstapp.ControlStickView;
+
+import org.w3c.dom.Text;
 
 import static android.provider.AlarmClock.EXTRA_MESSAGE;
 
@@ -22,6 +28,9 @@ public class MainActivity extends AppCompatActivity implements ControlStickView.
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         context = getApplicationContext();
         controlStickView = new ControlStickView(context);
         controlStickViewRight = new ControlStickViewRight(context);
